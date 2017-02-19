484a0efeb2a6a8ce4b9620e581c987b89f77a9d6
clara <clara@clara.com>
2017-02-19 10:05:26 -0600

    Add AJAX GET example

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,6 +4,13 @@ $(function(){
   // Add a listener for input text; listen for Enter key.
   // Send POST request to create new place
 
+  getAllPlaces();
+
+});
+
+
+function addNewPlaceFormListener() {
+
   $("#new_place").keypress(function(event){
     var placename = $(this).val();
     if (event.which == 13 && placename ) {   //if user presses Enter and $(this).val has a value
@@ -11,63 +18,109 @@ $(function(){
     }
   });
 
+}
+
+// Create elements
+
+function addPlacesToPage(places) {
+  //if (!places || places.length == 0){
+  //  //todo no places method
+  //}
+
+  var parent = $('#place_list');
+  for (var i = 0 ; i < places.length ; i++) {
+    addPlace(places[i], parent);
+  }
+
+  // Remove any old listeners, add listeners
 
   // Add listener to input checkboxes. As input checkboxes are checked and unchecked,
   // send PUT request to update the visited value of the place with the corresponding ID
-  $('.visited').click(addCheckListener);
-
+  $('.visited').off().click(checkListener);
 
   // Add listener to delete buttons. Send DELETE request to delete place when clicked.
-  $('.delete').click(addDeleteListener);
+  $('.delete').off().click(deleteListener);
 
-
-});
-
-// Listener functions, added to the existing elements ar the
-
-
-
-function addDeleteListener() {
-    var elem_id = $(this).attr('id');
-    var id = elem_id.replace('_delete', '');
-    deletePlace(id);
 }
 
 
-function addCheckListener() {
-  var visited = $(this).is(':checked');
-  var elem_id = $(this).attr('id');
-  var id = elem_id.replace('_is_visited', '');
-  updateVisited(id, visited);
+function addPlace(place, parent) {
+
+  var html = '<div id="' + place.id + '"><span class="placename">' + place.name + '</span><label class="visited_label" for="' + place.id + '_is_visited">Visited?</label>';
+
+  if (place.visited) {
+    html += '<span class="controls"><input class="visited" id="' + place.id + '_is_visited" type="checkbox" checked />'
+  }
+  else {
+    html += '<span class="controls"><input class="visited" id="' + place.id + '_is_visited" type="checkbox"/>'
+  }
+
+  html += '<button id="'+ place.id +'_delete" class="delete">Delete?</button></span></div>';
+
+  parent.append(html);
 }
+
+
+// Listener functions
+function deleteListener() {
+
+  $(this).text('Deleting...');              // Change button text to 'deleting...' Visual feedback for slower connections.
+  var elem_id = $(this).attr('id');         // Get the id of the element clicked, expected to be in the format '4_delete' for place id 4
+  var id = elem_id.replace('_delete', '');  // Cut off the _delete part, left with the number id
+  deletePlace(id);                          // Make AJAX request to delete the place with this ID
+}
+
+
+function checkListener() {
+  //todo feedback on updated?
+  var visited = $(this).is(':checked');   // Is the checkbox checked or unchecked?
+  var elem_id = $(this).attr('id');             // Get the checkbox id. For place with id 4, the id will be 4_visited
+  var id = elem_id.replace('_is_visited', '');   // Remove the _is_visited part
+  updateVisited(id, visited);                    // make AJAX request to update the place with this ID to the new visited state.
+}
+
 
 // These functions make AJAX calls
+
+function getAllPlaces(){
+
+  $.ajax({
+    method:"GET",
+    url:"/all"
+  }).done(function(data){
+    //Build HTML for each place in list
+    addPlacesToPage(data);
+    addNewPlaceFormListener();  //Once page is loaded, enable form
+
+  }).fail(function(error){
+    console.log("GET error");
+    console.log(error);
+  });
+
+}
+
 
 function addNewPlace(placename){
 
   $.ajax({
     method:"POST",
-    url:"/",
+    url:"/add",
     data: { "name" : placename }
   }).done(function(data){
 
     console.log('POST complete');
 
-    var html = '<div id="'+ data.id + '"><span class="placename">'+ placename +'</span> <label class="visited_label" for="' + data.id + '_is_visited">Visited?</label>' +
-    ' <input id="' + data.id + '_is_visited" class="visited" type="checkbox"> ' +
-      '<button id="'+ data.id + '_delete" class="delete">Delete?</button></div>';
-
-    $('#place_list').append(html);
     $('#new_place').val('');        // Clear input text box
+
+    var parent = $('#place_list');
+    addPlace(data, parent);
 
     // Update listeners
     var new_checkbox_id = '#' +data.id + '_is_visited';
     var new_delete_id = '#' +data.id + '_delete';
 
-    $(new_checkbox_id).click(addCheckListener);
-    $(new_delete_id).click(addDeleteListener);
-
-    //Alternative - remove all listeners with $('button').off() then reattach to all with $('button').click(function);
+    $(new_checkbox_id).click(checkListener);
+    $(new_delete_id).click(deleteListener);
 
 
   }).fail(function(error){
@@ -82,7 +135,7 @@ function updateVisited(id, visited) {
 
   $.ajax({
     method:"PUT",
-    url:"/",
+    url:"/update",
     data:{ "id":id, "visited":visited }
   }).done(function(){
     console.log('PUT complete');  // Could update the page here, if needed
@@ -97,7 +150,7 @@ function deletePlace(id) {
 
   $.ajax({
     method: "DELETE",
-    url: "/",
+    url: "/delete",
     data: { 'id': id }
   }).done(function (data) {
     console.log('DELETE complete');
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var router = express.Router();
 // "Database". Names of places, and whether the user has visited it or not.
 
 var places = [
-{id:"1", name:"Rome", visited:true},
-{id:"2", name:"New York", visited:false},
-{id:"3", name:"Tokyo", visited:false}
+{id: "1", name: "Rome", visited: true},
+{id: "2", name: "New York", visited: false},
+{id: "3", name: "Tokyo", visited: false}
 ];
 var counter = places.length;
 
@@ -18,8 +18,14 @@ router.get('/', function(req, res) {
 });
 
 
+/* GET all items home page. */
+router.get('/all', function(req, res) {
+  res.json(places);
+});
+
+
 /* POST - add a new location */
-router.post('/', function(req, res) {
+router.post('/add', function(req, res) {
 
   var name = req.body.name;
   var place = { 'id': ++counter + "" , 'name': name, 'visited': false };
@@ -32,13 +38,13 @@ router.post('/', function(req, res) {
   res.status(201);      // Created
   res.json(place);      // Send new object data back as JSON, if needed.
 
-  // TODO don't create if place already in list
+  // TODO may want to check if place already in list and don't add.
 
 });
 
 
 /* PUT - update whether a place has been visited or not */
-router.put('/', function(req, res){
+router.put('/update', function(req, res){
 
   var id = req.body.id;
   var visited = req.body.visited == "true";  // all the body parameters are strings
@@ -54,13 +60,12 @@ router.put('/', function(req, res){
   console.log('After PUT, the places list is');
   console.log(places);
 
-  res.status(200);    // OK
   res.json(place);
 
 });
 
 
-router.delete('/', function(req, res){
+router.delete('/delete', function(req, res){
 
   var place_id = req.body.id;
   console.log(place_id);
